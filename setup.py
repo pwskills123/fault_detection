3573dbe7c913bc06900c9f728844421e86e16702
pwskills123 <md.a@pw.live>
2024-10-16 12:49:19 +0530

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="pw skills",
+    author_email="md,a@pw.live",
+    packages=find_packages(),
+    install_requires=[],
+)
